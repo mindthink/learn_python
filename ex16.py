2c42f02429fff477b1f7b8a784bbeb3792163e5f
mindthink <270025453@qq.com>
2015-12-02 22:35:30 +0800

    add  copied.txt ex16.py ex16_1.py ex17.py test.txt

---
+++ ex16.py
@@ -0,0 +1,38 @@
+from sys import argv
+
+script,filename =argv
+
+print "We're going to erase %r."% filename
+print "If you dont' want that,hit CTRL-C(^C)."
+print "If you do want that,hit RETURN."
+
+raw_input("?")
+
+print "Opeing the file..."
+target=open(filename,'w')
+
+print "Truncating the file. Goodbye!"
+target.truncate()
+
+print "Now I'm going to ask you fo three lines."
+
+line1=raw_input("line 1:")
+line2=raw_input("line 2:")
+line3=raw_input("line 3:")
+
+print "I'm going to write these to the file."
+
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+target.write("\n")
+
+
+print "And finally ,we close it."
+print "line1 %r \n line2 %r \n line3 %r \n" %(target.write(line1),target.write(line2),target.write(line3))
+target.close()
+
+
+
